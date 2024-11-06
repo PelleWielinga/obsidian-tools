78a71355741a816da30fc125b3b1fc5a5b8700cb
Pelle Wielinga <pelle.wielinga@gmail.com>
2024-11-06 21:32:30 +0100

    Generated init

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "A simple Go package";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.05";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              cargo
+            ];
+          };
+        }
+      );
+    };
+}
